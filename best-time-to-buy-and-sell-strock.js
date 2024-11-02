25f4c8235ea862caa62bf0507adefab4f32e2355
Chanmi Lee <chanmi.kate.lee@gmail.com>
2024-11-02 15:15:48 +0900

    [Array] best-time-to-buy-and-sell-strock.js
    greedy algorithm

---
+++ best-time-to-buy-and-sell-strock.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let max = 0;
+    let start = prices[0]
+    let len = prices.length
+    for (let i = 1; i < len; i++) {
+        if (start < prices[i]) {
+            max += prices[i] - start
+        }
+        start = prices[i]
+    }
+    return max
+};
