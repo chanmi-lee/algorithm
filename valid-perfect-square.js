1833e2d670ae8508d84499b10e4381007759bf64
Chanmi Lee <chanmi.kate.lee@gmail.com>
2024-12-15 12:02:52 +0900

    [BinarySearch] valid-perfect-square.js

---
+++ valid-perfect-square.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/valid-perfect-square
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    let start = 1
+    let end = num
+    while (start <= end) {
+        let mid = Math.floor((start+end)/2)
+        if (mid * mid === num) {
+            return true
+        } else if (mid * mid > num) {
+            end = mid - 1
+        } else {
+            start = mid + 1
+        }
+    }
+    return false
+};
